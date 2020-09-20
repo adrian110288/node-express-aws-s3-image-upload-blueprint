640cd4e3608b13a776ede0ebeefca79d2b1de9e8
Adrian Lesniak <adrian110288@gmail.com>
2020-09-20 21:20:41 +0100

    PKs as UUID

--- models/Image.js
+++ models/Image.js
@@ -3,6 +3,11 @@ const sequelize = require('../models/index')
 const Store = require('./Store')
 
 const Image = sequelize.define('image', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
     name: {
         type: DataTypes.STRING
     },
--- models/Store.js
+++ models/Store.js
@@ -1,8 +1,13 @@
-const {DataTypes} = require('sequelize');
+const {Sequelize,DataTypes} = require('sequelize');
 const sequelize = require('../models/index')
 const Image = require('./Image')
 
 const Store = sequelize.define('store', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
+    },
     name: {
         type: DataTypes.STRING,
         allowNull: false
